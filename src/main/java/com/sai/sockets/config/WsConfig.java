e374b50a0c80202a2f0c71f180f474c67a2732ad
SaiAshish <saiashish3760@gmail.com>
2023-02-27 10:46:05 +0530

    Add files via upload

--- src/main/java/com/sai/sockets/config/WsConfig.java
+++ src/main/java/com/sai/sockets/config/WsConfig.java
@@ -13,7 +13,7 @@ public class WsConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/sai")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
